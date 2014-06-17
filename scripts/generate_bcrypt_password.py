bb7e7f2e813535c8d0635fef5df29623e9c99250
Andrew Lorente <hello@andrewlorente.com>
2014-06-16 20:51:52 -0700

    Require a password-confirm in digest generation [#1]
    Somewhat whimsical variable names chosen on the basis of being the same
    length.

--- scripts/generate_bcrypt_password.py
+++ scripts/generate_bcrypt_password.py
@@ -3,9 +3,19 @@
 import bcrypt
 import getpass
 
-passphrase = getpass.getpass("Enter a passphrase to bcrypt: ")
+def main():
+    passwonce = getpass.getpass("Enter a passphrase to bcrypt: ")
+    passtwice = getpass.getpass("Do it again, just to be safe: ")
 
-hashed = bcrypt.hashpw(passphrase, bcrypt.gensalt())
+    if passwonce == passtwice:
 
-print "Your bcrypted digest:"
-print hashed
+        hashed = bcrypt.hashpw(passwonce, bcrypt.gensalt())
+
+        print "Your bcrypted digest:"
+        print hashed
+    else:
+        print "They didn't match. Try again! No typos this time!"
+        main()
+
+if __name__ == '__main__':
+    main()
